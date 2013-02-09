bd53ae1ee0161392b30c82413050e60fef3a7287
Melanie Cebula <melaniecebula@berkeley.edu>
2013-02-09 13:18:48 -0800

    submit cat page

--- flaskthings.py
+++ flaskthings.py
@@ -1,9 +1,15 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/')
+def index():
+    return 'Rate My Cats'
+
+@app.route('/submit')
+def submit():
+    return 'Submit Your Cat'
 
 if __name__ == "__main__":
     app.run()
+
+app.run(debug=True)  #Debug mode
