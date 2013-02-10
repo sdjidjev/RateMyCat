cc6538cf1fac7c926e53f25dd1cdea31cf98c560
Melanie Cebula <melaniecebula@berkeley.edu>
2013-02-09 22:18:09 -0800

    raaaateeessssss

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, abort
 from flask.ext.pymongo import PyMongo
 from bson.objectid import ObjectId
+from pymongo import ASCENDING, DESCENDING
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = "cats"
@@ -14,7 +15,7 @@ mongo = PyMongo(app)
 
 @app.route('/')
 def index():
-    posts = mongo.db.posts.find()
+    posts = mongo.db.posts.find().sort("views", DESCENDING)
     return render_template('index.html', posts = posts)
 
 @app.route('/submit', methods=['GET', 'POST'])
@@ -32,6 +33,7 @@ def submit():
 @app.route('/cat/<post_id>')
 def show_post(post_id):
     post = mongo.db.posts.find_one({"_id" :ObjectId(post_id)})
+    update = mongo.db.posts.update({"_id" :ObjectId(post_id)},{ "$inc": { "views": 1 } } )
     if post is not None:
         return render_template('ratemycat.html', post=post)
     else:
